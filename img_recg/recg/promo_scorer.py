ae10dcefb5e64ab1fa1615f0c3495c9cee71cb51
Milosz Blasiak <milblasiak@gmail.com>
2019-01-12 23:30:05 +0100

    Add txt scoring function and update key_words

---
+++ img_recg/recg/promo_scorer.py
@@ -0,0 +1,15 @@
+import re
+
+
+def score_promo(text: str, scoring_words):
+    file = open(scoring_words, "r")
+    word_types = file.read().split('\n')
+    score = 0
+    for word_type in word_types:
+        words = word_type.split(' ')
+        for word in words:
+            if word in text:
+                score += 10
+                break
+
+    return score
