58cc54add23ddce93e8c2524abacc08a515327b3
Milosz Blasiak <milblasiak@gmail.com>
2019-01-12 17:44:33 +0100

    Add some test photos

--- img_recg/simple_recg.py
+++ img_recg/simple_recg.py
@@ -3,4 +3,4 @@ import PIL
 import pytesseract
 
 tessdata_dir_config = r'--tessdata-dir "train_data"'
-print(pytesseract.image_to_string(PIL.Image.open('resources/orzo_year.png'),lang='pol',config=tessdata_dir_config))
+print(pytesseract.image_to_string(PIL.Image.open('resources/orzo_year.png'), lang='pol', config=tessdata_dir_config))
