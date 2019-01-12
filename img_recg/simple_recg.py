c920ad8b91f6af9d4d435e9600842b0819c750d3
Milosz Blasiak <milblasiak@gmail.com>
2019-01-12 17:24:00 +0100

    Add Polish language to OCR

--- img_recg/simple_recg.py
+++ img_recg/simple_recg.py
@@ -2,4 +2,5 @@ import PIL
 
 import pytesseract
 
-print(pytesseract.image_to_string(PIL.Image.open('resources/orzo_year.png')))
+tessdata_dir_config = r'--tessdata-dir "train_data"'
+print(pytesseract.image_to_string(PIL.Image.open('resources/orzo_year.png'),lang='pol',config=tessdata_dir_config))
