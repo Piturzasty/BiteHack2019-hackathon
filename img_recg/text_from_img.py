e29e94779e24b97339a89a07bcec7dce100f8250
Milosz Blasiak <milblasiak@gmail.com>
2019-01-13 00:14:24 +0100

    Cleanup file structer

--- img_recg/text_from_img.py
+++ img_recg/text_from_img.py
@@ -4,7 +4,7 @@ from google.oauth2 import service_account
 
 
 def detect_text(path, is_url=False):
-    credentials = service_account.Credentials.from_service_account_file('../keys/key.json')
+    credentials = service_account.Credentials.from_service_account_file('./keys/key.json')
 
     client = vision.ImageAnnotatorClient(credentials=credentials)
     if is_url:
@@ -21,6 +21,3 @@ def detect_text(path, is_url=False):
     # Retrun whole text
     return texts[0].description
 
-
-print(detect_text(
-    'https://scontent.xx.fbcdn.net/v/t1.0-9/q81/p720x720/49947322_347544445874330_5586516966098599936_o.jpg?_nc_cat=109&_nc_ht=scontent.xx&oh=1f2a8eacc4cfc3632b00f4a3d46b52b2&oe=5CC12D99',True))
