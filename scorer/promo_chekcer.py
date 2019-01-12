776ac9e1565a40812902960963fe266365ee8eb1
Milosz Blasiak <milblasiak@gmail.com>
2019-01-13 00:26:07 +0100

    Add function checking if post is promo

---
+++ scorer/promo_chekcer.py
@@ -0,0 +1,13 @@
+from .promo_scorer import score_promo
+from img_recg.text_from_img import detect_text
+
+
+def is_promo(promo):
+    word_list = 'key_words.txt'
+    text_score = score_promo(promo[2], word_list)
+    photo_score = 0
+    if promo[3] != '#':
+        text = detect_text(promo[3], True)
+        photo_score = score_promo(text, word_list)
+
+    return text_score + photo_score
