d77020284ef9dce05f19c192a122a38a1c410e8d
Krzysztof Geleta <homar425@gmail.com>
2019-01-13 01:50:52 +0100

    fixed path

--- scorer/promo_chekcer.py
+++ scorer/promo_chekcer.py
@@ -5,6 +5,7 @@ from img_recg.text_from_img import detect_text
 def is_promo(promo):
     word_list = 'C:\\Users\\Leslie\\Documents\\hackaton\\BiteHack2019-hackathon\\scorer\\key_words.txt'
     text_score = score_promo(promo[1], word_list)
+    print(promo[1])
     photo_score = 0
     if promo[2] != '#':
         text = detect_text(promo[2], True)
