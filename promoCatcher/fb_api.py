b3c4f454edafe51b2e5c6f8b31fc450c46349ccd
Krzysztof Geleta <homar425@gmail.com>
2019-01-13 01:12:05 +0100

    added api for Facebook posts

---
+++ promoCatcher/fb_api.py
@@ -0,0 +1,28 @@
+import facebook
+import datetime
+
+
+file = open('fb_token', "r")
+token = file.read()
+graph = facebook.GraphAPI(access_token=token)
+
+id = '347472895881485'
+
+
+def fromFacebook(id):
+    posts_array = []
+    posts = graph.request(id + '/posts')
+    for elem in posts['data']:
+        post_time = datetime.datetime.strptime(elem['created_time'], '%Y-%m-%dT%H:%M:%S+%f')
+        if datetime.datetime.now() - post_time < datetime.timedelta(days=7):
+            picture = graph.get_object(id=elem['id'], fields='full_picture')
+            try:
+                picStr = picture['full_picture']
+            except KeyError:
+                picStr = "#"
+            posts_array.append((elem['created_time'], elem['message'], picStr))
+    return posts_array
+
+
+test = fromFacebook(id)
+print(test)
