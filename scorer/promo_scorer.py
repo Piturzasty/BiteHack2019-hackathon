bf0d5f8fe3115feb7fff28ba90c13d49c22b09b7
Milosz Blasiak <milblasiak@gmail.com>
2019-01-13 04:47:13 +0100

    Make scoring case insensitive

--- scorer/promo_scorer.py
+++ scorer/promo_scorer.py
@@ -1,14 +1,13 @@
-import re
-
 
 def score_promo(text: str, scoring_words):
     file = open(scoring_words, "r")
     word_types = file.read().split('\n')
     score = 0
     for word_type in word_types:
-        words = word_type.split(' ')
+        words = word_type.split('-')
         for word in words:
-            if word in text:
+            if word in text.lower():
+
                 score += 10
                 break
 
